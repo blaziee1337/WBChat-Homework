005afe94027dfea6bfaf4949b00659a2db1ad8fe
Halil Yavuz <halil9913@gmail.com>
2024-06-14 22:33:24 +0300

    Initial Commit

---
+++ WBChat-Homework/WBChat_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBChat_HomeworkApp.swift
+//  WBChat-Homework
+//
+//  Created by Halil Yavuz on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBChat_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
