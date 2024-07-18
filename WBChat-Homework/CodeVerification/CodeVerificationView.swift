a192774faba55eb7b032e37080e54a8eb5cb0593
Halil Yavuz <halil9913@gmail.com>
2024-07-18 10:55:02 +0300

    added NavigationLink to CodeVerificationView

---
+++ WBChat-Homework/CodeVerification/CodeVerificationView.swift
@@ -0,0 +1,19 @@
+//
+//  CodeVerificationView.swift
+//  WBChat-Homework
+//
+//  Created by Halil Yavuz on 18.07.2024.
+//
+
+import SwiftUI
+
+struct CodeVerificationView: View {
+    let phoneNumber: String
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodeVerificationView()
+}
