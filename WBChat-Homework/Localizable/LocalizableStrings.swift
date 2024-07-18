72f68518f5bc8b515958e1899e808601ceafbb00
Halil Yavuz <halil9913@gmail.com>
2024-07-18 21:33:48 +0300

    added localizable

---
+++ WBChat-Homework/Localizable/LocalizableStrings.swift
@@ -0,0 +1,16 @@
+//
+//  LocalizableStrings.swift
+//  WBChat-Homework
+//
+//  Created by Halil Yavuz on 18.07.2024.
+//
+
+import Foundation
+
+struct LocalizedStrings {
+    static var enterCode: String { NSLocalizedString("enter_code", comment: "") }
+    static var sentCodeToNumber: String { NSLocalizedString("confirmation_code_message", comment: "") }
+    static var yourCode: String { NSLocalizedString("your_code", comment: "") }
+    static var incorrectCodeTryAgain: String { NSLocalizedString("incorrect_code_try_again", comment: "") }
+    static var requestCodeAgain: String { NSLocalizedString("request_code_again", comment: "") }
+}
