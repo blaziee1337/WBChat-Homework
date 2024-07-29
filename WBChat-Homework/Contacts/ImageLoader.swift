81add5d56886b8ea8cadf812f100bd6368f6e2cd
Halil Yavuz <halil9913@gmail.com>
2024-07-29 19:36:21 +0300

    added ImageLoader

--- WBChat-Homework/Contacts/ImageLoader.swift
+++ WBChat-Homework/Contacts/ImageLoader.swift
@@ -5,4 +5,18 @@
 //  Created by Halil Yavuz on 29.07.2024.
 //
 
-import Foundation
+import SwiftUI
+
+class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+    
+    func loadImage(from name: String) {
+        DispatchQueue.global().async {
+            let image = UIImage(named: name)
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+    }
+}
+
